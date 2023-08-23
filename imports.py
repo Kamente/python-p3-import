b4bb21cb9dea3c00aba3c287fa03db197a9c7eb8
Kamente <justinm48000@gmail.com>
2023-08-23 22:52:34 +0300

    done

---
+++ imports.py
@@ -0,0 +1,3 @@
+# Absolute imports: specifies the module to be imported using the full path
+# relative imports: Allows for rearrange of the structure of the large pkgs without having to edit sub-pkgs
+
